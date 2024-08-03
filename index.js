953fd438de4c6e4c60c2861ceae5f72d2b6fa237
Michihiro Kawada <m@llll.jp>
2024-08-04 07:02:50 +0900

    handleClick, handlePress実装

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 
 const drums = document.querySelectorAll(".drum");
 const num = drums.length;
+
 for (i = 0; i < num; i++) {
-  drums[i].addEventListener("click", handle)
+  drums[i].addEventListener("keypress", handlePress)
+  drums[i].addEventListener("click", handleClick)
 }
 
-function handle() {
-  const audio = new Audio('sounds/tom-1.mp3');
-  audio.play();
+function handlePress(event) {
+  makesound(event.key);
+}
 
+function handleClick() {
+  makesound(this.innerHTML);
+}
+
+function makesound(key) {
+  console.log(key)
+  let audio;
+  switch (key) {
+    case "w":
+      audio = new Audio('sounds/tom-1.mp3');
+      break;
+    case "a":
+      audio = new Audio('sounds/snare.mp3');
+      break;
+    case "s":
+      audio = new Audio('sounds/kick-bass.mp3');
+      break;
+    case "d":
+      audio = new Audio('sounds/tom-1.mp3');
+      break;
+    case "j":
+      audio = new Audio('sounds/tom-2.mp3');
+      break;
+    case "k":
+      audio = new Audio('sounds/tom-3.mp3');
+      break;
+    case "l":
+      audio = new Audio('sounds/tom-4.mp3');
+      break;
+    default:
+      console.log("invalid key");
+      return;
+  }
+  audio.play();
 }
---
+++ index2.js
@@ -0,0 +1,8 @@
+
+var bellboy1 = {
+  name: "Timmy",
+  age: 19,
+  language: ["English,French"]
+}
+
+console.log(bellboy1.language)
